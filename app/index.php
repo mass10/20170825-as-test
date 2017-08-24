1b3578f4841cfbe76e0306f137fad5bf6f6f321b
mass10 <mass10.github@gmail.com>
2017-08-24 22:12:27 +0900

    wip

--- app/index.php
+++ app/index.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 ini_set("display_errors", 1);
+date_default_timezone_set('Asia/Tokyo');
 
 print('IP ADDRESS: [');
 print($_SERVER['SERVER_ADDR']);
