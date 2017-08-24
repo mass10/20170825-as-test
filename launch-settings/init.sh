2b4e1be7b4fd766e51be2e886352e6fcf665b58d
mass10 <mass10.github@gmail.com>
2017-08-24 21:54:32 +0900

    wip

--- launch-settings/init.sh
+++ launch-settings/init.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
+
 yum install httpd -y
 yum install mod_ssl -y
+
 chkconfig httpd on
+
 service httpd start
+
+mkdir -p /var/www/html/
+wget https://raw.githubusercontent.com/mass10/20170825-as-test/master/app/index.php --output-document /var/www/html/index.php
+
 date >> /tmp/init-sh-log
 echo '*** READY ***' >> /tmp/init-sh-log
 
