dcba038709babda2320bef2becaf8e06aac4b6ac
mass10 <mass10.github@gmail.com>
2017-08-24 20:53:02 +0900

    wip

--- launch-settings/init.sh
+++ launch-settings/init.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
 yum install httpd -y
 yum install mod_ssl -y
 chkconfig httpd on
 service httpd start
+date >> /tmp/init-sh-log
+echo '*** READY ***' >> /tmp/init-sh-log
 
