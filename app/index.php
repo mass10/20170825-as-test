e52b080beb682cf649e849f85d8ede26eba34e80
mass10 <mass10.github@gmail.com>
2017-08-24 21:58:17 +0900

    wip

--- app/index.php
+++ app/index.php
@@ -1,4 +1,12 @@
 <?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+print('IP ADDRESS: [');
+print($_SERVER['SERVER_ADDR']);
+print(']<br>');
+print("\n");
+print('TIMESTAMP: [');
 print(date(DATE_ATOM));
+print(']');
 ?>
 
